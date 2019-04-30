c48a82bd3052b9c7e080e7388dcaab06c1f0d124
Pedro <pedro_0797@hotmail.com>
2019-04-30 18:10:52 -0500

    Plantilla de configuración

--- SimonDice.X/SimonDice.asm
+++ SimonDice.X/SimonDice.asm
@@ -1,40 +1,72 @@
-    RADIX DEC ; SET DECIMAL AS DEFAULT BASE 
-    PROCESSOR 18F45K50 ; SET PROCESSOR TYPE 
+    RADIX DEC
+    PROCESSOR 18F45K50
     #INCLUDE <P18F45K50.INC> 
-; 
-;
-; VARIABLE'S DEFINITION SECTION 
-; 
-; * ONLY NEEDED FOR SOFTWARE SIMULATION * 
-; 
-    org 0x00 ; RESET VECTOR 
+    
+    ; Variable definition
+puntaje EQU 0x00
+indice EQU 0x01
+    
+    ; Code for software simulation
+    org 0x00	    ; Reset vector 
     goto 0X1000 
-; 
-    org 0X08 ; HIGH INTERRUPT VECTOR 
+ 
+    org 0X08	    ; High Interrupt Vector 
     goto 0X1008 
-; 
-    org 0X18 ; LOW INTERRUPT VECTOR 
+ 
+    org 0X18	    ; Low Interrupt Vector 
     goto 0X1018 
-; 
-; * END OF CODE FOR SOFTWARE SIMULATION * 
-; 
-; 
-; * START OF PROGRAM * 
-; JUMP VECTORS 
-; 
-    org 0X1000 ; RESET VECTOR 
+    ; End of code for software simulation
+    
+    org 0X1000	    ; Reset Vector 
     goto main
-    org 0X1008 ; HIGH INTERRUPT VECTOR 
-    ;goto isrHigh ; UNCOMMENT WHEN NEEDED 
-    org 0X1018 ; LOW INTERRUPT VECTOR 
-    ;goto isrLow ; UNCOMMENT WHEN NEEDED 
+    org 0X1008	    ; High Interrupt Vector 
+    ;goto isrHigh   
+    org 0X1018	    ; Low Interrupt Vector 
+    ;goto isrLow
 
-; 
-; RESOURCE INITIALIZATION 
-; 
 main:
+    movlb 15
+    clrf ANSELA, BANKED
+    clrf TRISA, A
+    clrf LATA, A
+    
+    clrf ANSELB, BANKED
+    bcf INTCON2, 7, A	    ; Enable global pull-up
+    movlw b'00001111'
+    movwf TRISB, A
+    movwf WPUB, A
+    setf LATB, A
+    
+    clrf ANSELC, BANKED
+    clrf TRISC, A
+    clrf LATC, A
+    
+    clrf ANSELD, BANKED
+    clrf TRISD, A
+    clrf LATD, A
     
     
 loop:
     
+loadEEPROM:
+    
+menuLCD:
+    
+configT2:
+    
+jugar:
+    
+ciclo1:
+    
+ciclo2:
+    
+incorr:
+    
+corr:
+    
+puntaje:
+    
+    
+    
     end
+    
